a8078ca346de6aec802b4ea8ff6ec5e48227a22b
antoo69 <antobarry69@gmail.com>
2024-10-01 15:07:29 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,18 +5,18 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 
 
-API_ID = int(getenv("API_ID", "5655010")) #optional
-API_HASH = getenv("API_HASH", "137d3d82e79c41f113ba640fc3cb2dfd") #optional
+API_ID = int(getenv("API_ID", "28524972")) #optional
+API_HASH = getenv("API_HASH", "409130d1d167a2c26862e65e8779a3d6") #optional
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = getenv("OWNER_ID")
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC")
-ALIVE_TEXT = getenv("ALIVE_TEXT")
-PM_LOGGER = getenv("PM_LOGGER")
-OPENAI_API = getenv("OPENAI_API")
+OWNER_ID = getenv("OWNER_ID", "6334438071")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://ferdisyrl:buburayam1@cluster0.89myp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+BOT_TOKEN = getenv("BOT_TOKEN", "7856088201:AAE6kjerf0uAAl_8gkxH2V9um1AEa5SBRYg")
+ALIVE_PIC = getenv("ALIVE_PIC", https://envs.sh/PHS.jpg"")
+ALIVE_TEXT = getenv("ALIVE_TEXT", "halo pantek!! Userbot aktif")
+PM_LOGGER = getenv("PM_LOGGER"), or 0)
+OPENAI_API = getenv("OPENAI_API", "")
 RMBG_API = getenv("RMBG_API", "3RCCWg8tMBfDWdAs44YMfJmC")
-BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
+BOTLOG_CHATID = int(getenv("BOTLOG_CHATID", "-1002456606377")
 PM_LIMIT = int(getenv("PM_LIMIT") or 5)
 BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
@@ -25,7 +25,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 BRANCH = getenv("BRANCH", "main") #don't change
 CMD_HNDLR = getenv("CMD_HNDLR", ".")
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "BQGzQawATmQxqUy8QODS41yS8koHOIfWS0TDuMoDuLT0dIyazNKF8HmXoanLjXj3hd4Mk6Y97KN5TFgxSnCPkDrlTyntpGEL_aHHWwLtST2OxNcwAFj0a9U1WU1B01929NBdA5vg5mT4ruKK8e4gHViBXXZYpVBFUBzsePgfSm9X3l52capl1KYkP744r7k61Dn8uGM75GhPQJkrS7kpqg0cxlE2wVmM3plJBqw7QOy6WOYIGpq4HAVd6xGCc6Ai3TU7EaNGqhik6Q5vCDqQBxdpme7dXPWmg_VMNtOTDS4IEqu-IzXSe3IqW6nIGihnt4dA3xOlOQuruS020bbUvSLC0sSk3wAAAAF5j9q3AA")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
