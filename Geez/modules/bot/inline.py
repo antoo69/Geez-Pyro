0c73461405c754f2a5a33c7cb48e1d0c514600e4
antoo69 <antobarry69@gmail.com>
2024-10-02 09:20:20 +0700

    Update inline.py

--- Geez/modules/bot/inline.py
+++ Geez/modules/bot/inline.py
@@ -71,7 +71,7 @@ async def get_readable_time(seconds: int) -> str:
 async def alive_function(message: Message, answers):
     uptime = await get_readable_time((time.time() - StartTime))
     msg = f"""
-<b> Geez-Pyro™ </b>
+<b> Ferdi-Userbot </b>
 
 <b> • User :</b> {message.from_user.mention}
 <b> • Plugins :</b> <code>{len(CMD_HELP)} Modules</code>
